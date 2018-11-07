250daa4b4573fa49579027835673a24c2645d17c
fluke <fhluky@gmail.com>
2018-11-07 13:43:09 +0700

    first commit

---
+++ src/main/java/th/co/rocinante/RocinanteApplication.java
@@ -0,0 +1,12 @@
+package th.co.rocinante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocinanteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RocinanteApplication.class, args);
+	}
+}
---
+++ src/main/java/th/co/rocinante/controller/WebAppController.java
@@ -0,0 +1,13 @@
+package th.co.rocinante.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebAppController {
+
+	@RequestMapping("/")
+	public String index(){
+		return "index";
+	}
+}
