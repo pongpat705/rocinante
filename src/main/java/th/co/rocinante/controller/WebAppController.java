bde32e2c68a13c28f5e53078e77c9f6e27a5d99c
fluke <fhluky@gmail.com>
2018-11-07 15:10:36 +0700

    upload

--- src/main/java/th/co/rocinante/controller/WebAppController.java
+++ src/main/java/th/co/rocinante/controller/WebAppController.java
@@ -3,6 +3,7 @@ package th.co.rocinante.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -48,6 +49,13 @@ public class WebAppController {
 		model.addAttribute("result", result);
 		
 		return "index";
+	}
+	
+	@GetMapping("/upload")
+	public String uploadPage(){
+		
+		
+		return "upload";
 	}
 	
 	
